64055be3489adcb475d2762f8ea0fa180a066517
sunkey <614053620@qq.com>
2021-06-02 19:25:24 +0800

    base

--- src/main/java/sunkey/java/util/Map.java
+++ src/main/java/sunkey/java/util/Map.java
@@ -1,8 +1,51 @@
 package sunkey.java.util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.concurrent.*;
+
 /**
  * @author Sunkey
  * @since 2021-05-26 11:43 上午
  **/
 public class Map {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+    }
+
+    static void lists() {
+        ArrayList l;
+        LinkedList ll;
+        CopyOnWriteArrayList cwl;
+    }
+
+    static void maps() {
+        HashMap<String, String> hm = new HashMap<>(/*loadFactor, initialCapacity, collection*/);
+        // LRU = true
+        LinkedHashMap<String, String> lhm = new LinkedHashMap<>(/*+!accessOrder*/);
+        // concurrencyLvl: 预估并发数，cap=cap<cLvl?cLvl:cap
+        ConcurrentHashMap<String, String> chm = new ConcurrentHashMap<>(/*cap,fctr,concurrencyLvl*/);
+    }
+
+    static void queues() throws Exception {
+        LinkedBlockingQueue q = new LinkedBlockingQueue<>(10);
+
+        q.add(""); //throw
+        q.remove("");//bool
+
+        q.offer(""); // bool
+        q.poll();// null
+
+        q.put(""); // block
+        q.take();// block
+
+        LinkedTransferQueue ltq = new LinkedTransferQueue<>();
+        ArrayBlockingQueue abq = new ArrayBlockingQueue<>(10);
+        PriorityBlockingQueue pbq = new PriorityBlockingQueue();
+    }
+
 }
--- src/main/java/sunkey/java/util/concurrent/Locks.java
+++ src/main/java/sunkey/java/util/concurrent/Locks.java
@@ -1,8 +1,31 @@
 package sunkey.java.util.concurrent;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * @author Sunkey
  * @since 2021-05-26 11:43 上午
  **/
 public class Locks {
+
+    public static void main(String[] args) {
+
+        // Fair/UnFair : 公平锁：按照等待顺序，影响性能
+        // tryLock() 忽略公平锁影响
+        // 可重入互斥锁
+        ReentrantLock rl = null;
+        // Condition等同于Object.wait/notify
+        // rl.lock()/unlock() = sync(object){}
+        Condition cond = rl.newCondition();
+        // object.wait/notify = cond.await() / cond.signal()/cond.signalAll()
+
+        // 锁支持降级从W到R
+        // 注：从R升级到W永远不会成功，必须先释放
+        ReentrantReadWriteLock rwl;
+
+    }
+
+
 }
